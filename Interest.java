562fc2913034b79f69808bdf78c413cce0c50322
GustavoTaniwaki <gustavokstaniwaki@gmail.com>
2024-10-16 19:27:05 -0300

    User e Good prontos
    junto de outras classes vazias

---
+++ Interest.java
@@ -0,0 +1,5 @@
+package com.troca.troca;
+
+public class Interest {
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package com.troca.troca;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
