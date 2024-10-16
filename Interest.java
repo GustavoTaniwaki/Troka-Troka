39d36f42baa355752a8b1ae833a69559782e7325
Kajuero <66973158+Kajuero@users.noreply.github.com>
2024-10-16 20:38:36 -0300

    Update Interest.java

--- Interest.java
+++ Interest.java
@@ -1,5 +1,14 @@
 package com.troca.troca;
 
-public class Interest {
+import java.io.Serializable;
+import java.util.List;
 
+public class Interest {
+  private Serializable id;
+  private Float value;
+  private String category;
+  private String conservationState;
+  private String addres;
+  private List<String> keyworlds = new List<>();
+  private User user;
 }
