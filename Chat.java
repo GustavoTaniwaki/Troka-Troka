7562160aaf670ee56d677ea54bae5f83361a6c87
Kajuero <66973158+Kajuero@users.noreply.github.com>
2024-10-20 21:42:58 -0300

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -3,6 +3,12 @@ package com.troca.troca;
 import java.util.List;
 import java.util.ArrayList;
 
+import lombok.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+
 public class Chat {
   private List<Message> ownership = new ArrayList<>();
 }
