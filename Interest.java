ace82fd1d030cac94c9ec260d9ae831ac183e5ee
Kajuero <66973158+Kajuero@users.noreply.github.com>
2024-10-20 21:43:33 -0300

    Update Interest.java

--- Interest.java
+++ Interest.java
@@ -3,7 +3,15 @@ package com.troca.troca;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
 public class Interest {
+  @Id
   private Serializable id;
   private Float value;
   private String category;
