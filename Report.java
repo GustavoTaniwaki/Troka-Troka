90c9981ef9622b385f99713fe5cbfc10655fa938
Kajuero <66973158+Kajuero@users.noreply.github.com>
2024-10-20 21:46:33 -0300

    Update Report.java

--- Report.java
+++ Report.java
@@ -3,7 +3,15 @@ package com.troca.troca;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
 public class Report {
+    @Id
     private User reportingClient;
     private User reportedClient;
     private Trade trade;
