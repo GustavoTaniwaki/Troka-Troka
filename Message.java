395b2143531560116f65a110b46712088e4c34c2
Kajuero <66973158+Kajuero@users.noreply.github.com>
2024-10-20 21:43:58 -0300

    Update Message.java

--- Message.java
+++ Message.java
@@ -2,7 +2,15 @@ package com.troca.troca;
 
 import java.util.Date;
 
+import lombok.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
 public class Message {
+  @Id
   private User user;
   private String message;
   private Date dateM;
